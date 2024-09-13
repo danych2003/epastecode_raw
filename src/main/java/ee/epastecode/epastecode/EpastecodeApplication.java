5d69c4f1df2d8e8f44666a343a57d4427ea405ba
danych <danychit@outlook.com>
2024-09-13 18:21:49 +0300

    init

---
+++ src/main/java/ee/epastecode/epastecode/EpastecodeApplication.java
@@ -0,0 +1,13 @@
+package ee.epastecode.epastecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpastecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpastecodeApplication.class, args);
+	}
+
+}
